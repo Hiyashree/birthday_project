6281ead77a820e3b5556ce340c2dbecf7c7e98d5
Hiyashree <hiyasarma90@gmail.com>
2025-06-20 19:58:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const cors = require('cors');
+app.use(cors());
+
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cron = require('node-cron');
